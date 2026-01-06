f7491386efbd9b09249238d1eab3827068ec292e
kunwarpramod <kunwarpramod225@gmail.com>
2026-01-06 12:51:31 +0545

    Update Main.java with new  code

--- src/studentmanagement/Main.java
+++ src/studentmanagement/Main.java
@@ -1,5 +1,16 @@
 package studentmanagement;
 
+//Main.java
 public class Main {
-
+ public static void main(String[] args) {
+     System.out.println("Student Management System - Week 8 Practice");
+     
+     // create a student and print their info
+     Student s = new Student("pramod", 20);
+     System.out.println("Name: " + s.getName() + ", Age: " + s.getAge());
+     GradeCalculator gc = new GradeCalculator();
+     int[] grades = {85, 92, 78}; double avg = gc.calculateAverage(grades); 
+     System.out.println("Average: " + avg + " | Letter: " + gc.toLetterGrade(avg));
+     
+ }
 }
