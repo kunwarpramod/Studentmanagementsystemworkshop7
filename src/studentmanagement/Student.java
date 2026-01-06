7193707a93f69b053fcc578bf3120eeaf5f5120d
kunwarpramod <kunwarpramod225@gmail.com>
2026-01-06 12:12:54 +0545

    Initial project setup with Main.java and Student.java (constructor only)

---
+++ src/studentmanagement/Student.java
@@ -0,0 +1,13 @@
+package studentmanagement;
+
+//Student.java
+public class Student {
+ private String name;
+ private int age;
+
+ public Student(String name, int age) {
+     this.name = name;
+     this.age = age;
+ }
+ // Getters to be added later
+}
